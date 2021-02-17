0dc6948150e8b779a1798ff9e656384c2dc9eb80
Juan <juansfontz@gmail.com>
2021-02-16 20:39:15 -0400

    feat: Add API fetch

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import Layout from './components/Layout'
 import Wrapper from './components/Wrapper'
 import './App.css';
 
-const quote = "Quality means doing it right when no one is looking."
-const author = "Henry Ford"
-
 function App() {
   return (
     <Layout>
-      <Wrapper quote={quote} author={author} />
+      <Wrapper />
     </Layout>
   );
 }
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Wrapper = (props) => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [quotes, setQuotes] = useState([])
+  const [author, setAuthor] = useState('')
+
+  useEffect( () => {
+    const API_URL = "https://quote-garden.herokuapp.com/";
+    const API_VERSION = "api/v3/";
+
+    const fetchData = async () => {
+      if (!isLoading) return
+
+      const data = await fetch(`${API_URL}${API_VERSION}quotes/random`)
+      const res = await data.json()
+
+      console.log(res.data[0])
+      setQuotes(res.data[0].quoteText)
+      setAuthor(res.data[0].quoteAuthor)
+      setIsLoading(false)
+    }
+
+    fetchData()
+  }, [isLoading])
+
   return (
     <section>
-      <figure>
-        <blockquote>
-          <p>{props.quote}</p>
-        </blockquote>
-        <figcaption>— {props.author}</figcaption>
-      </figure>
+      {!isLoading &&
+        <figure>
+          <blockquote>
+            <p>{quotes}</p>
+          </blockquote>
+          <figcaption style={{textAlign: 'right'}}>— {author}</figcaption>
+        </figure>
+      }
+      {isLoading &&
+        <p>Loading...</p>
+      }
     </section>
   )
 }
