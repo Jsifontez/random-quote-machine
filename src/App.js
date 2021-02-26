a9a6df44bdc1ff00a3bbbd8ee83ce1182f541576
Juan <juansfontz@gmail.com>
2021-02-26 01:27:33 -0400

    feat: Add footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Layout from './components/Layout'
 import Wrapper from './components/Wrapper'
+import Footer from './components/Footer/Footer'
 import './App.css';
 
 function App() {
   return (
-    <Layout>
-      <Wrapper />
-    </Layout>
+    <>
+      <Layout>
+        <Wrapper />
+      </Layout>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer () {
+  return (
+    <footer className="footer">
+      <p>
+          Code with ❤️ by <a href="https://twitter.com/jsifontez_" target="_blank" rel="noreferrer">@jsifontez</a>
+        </p>
+        <p>
+          Commits are welcome on <a href="https://github.com/Jsifontez/country-quiz-devchallenge" target="_blank" rel="noreferrer">GitHub</a>
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
